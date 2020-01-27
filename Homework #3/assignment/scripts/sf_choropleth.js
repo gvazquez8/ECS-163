4285dc90c188cbcdce6912ed2718c924083efcd4
gvazquez8 <gabvazquez@ucdavis.edu>
2020-01-27 03:33:18 -0800

    Made Progress in HW3

--- Homework #3/assignment/scripts/sf_choropleth.js
+++ Homework #3/assignment/scripts/sf_choropleth.js
@@ -1,5 +1,5 @@
-const uniqueScheme = ["#fff7ec","#fee8c8","#fdd49e","#fdbb84","#fc8d59","#ef6548","#d7301f"]  
-
+const uniqueScheme = ["#fff7ec","#fee8c8","#fdd49e","#fdbb84","#fc8d59","#ef6548","#d7301f"]
+var highlighted_district = undefined;
 // load data with queue
 var url1 = "./data/neighborhood.geojson";
 var url2 = "./data/listing_count.json";
@@ -26,6 +26,10 @@ function draw(error, data) {
     .domain([1, 25, 50, 100, 200, 300, 700])
     .range(uniqueScheme);
 
+  var thresh = d3.scaleThreshold()
+    .domain([1, 25, 50, 100, 200, 300, 700])
+    .range()
+
   // create a projection properly scaled for SF
   var projection = d3.geoMercator()
     .center([-122.433701, 37.767683])
@@ -43,7 +47,19 @@ function draw(error, data) {
     .append('path')
     .attr('d', path)
     .style('stroke', 'black')
-    .style('stroke-width', 1);
+    .style('stroke-width', 1)
+    .on("mouseover", (datum, index, nodes) => {
+      showTooltip(datum, index, nodes);
+    })
+    .on("mousemove", (datum, index, nodes) => {
+      moveTooltip(datum, index, nodes);
+    })
+    .on("mouseleave", (datum, index, nodes) => {
+      hideTooltip(datum, index, nodes);
+    })
+    .on("click", (datum, index, nodes) => {
+      selectDistrict(datum, index, nodes);
+    })
 
   // // normalize neighborhood names
   map.datum(function(d) {
@@ -65,16 +81,6 @@ function draw(error, data) {
       return color(d.count);
     })
     .attr("transform", "translate(60" + ", 50" + ")")
-    .on("mouseover", (datum, index, nodes) => {
-      showTooltip(datum, index, nodes);
-    })
-    .on("mousemove", (datum, index, nodes) => {
-      moveTooltip(datum, index, nodes);
-    })
-    .on("mouseleave", (datum, index, nodes) => {
-      hideTooltip(datum, index, nodes);
-    })
-
 }
 
 
@@ -82,25 +88,29 @@ function showTooltip(datum, index, nodes) {
   // Initialize tooltip when user mouses over district on map
 
   var tooltip = d3.select("#mapTooltip");
-
   tooltip.style("left", d3.event.pageX + "px");
   tooltip.style("top", d3.event.pageY + "px");
   tooltip.style("visibility", "visible");
-  tooltip.text(datum.properties.neighbourhood + "\n" + datum.count);
+  tooltip.html("<b>" + datum.properties.neighbourhood.replace(/_/g, " ") + "</b>\n" + datum.count + "</span>");
 }
-
 function moveTooltip(datum, index, nodes) {
   // Move tooltip as mouse moves while over district
-
   var tooltip = d3.select("#mapTooltip");
-
   tooltip.style("left", (d3.event.pageX )+ "px");
   tooltip.style("top", (d3.event.pageY) + "px");
 }
-
 function hideTooltip(datum, index, nodes) {
-
   var tooltip = d3.select("#mapTooltip");
-
   tooltip.style("visibility", "hidden");
+}
+function selectDistrict(datum, index, nodes) {
+ // d3.select(this).style("opacity", 0.2);
+ if (nodes[index] === highlighted_district) {
+  d3.select("#map").selectAll("path").style("opacity",1);
+  highlighted_district = undefined;
+  return;
+ }
+ d3.select("#map").selectAll("path").style("opacity",0.2);
+ d3.select(nodes[index]).style("opacity",1);
+ highlighted_district = nodes[index];
 }
